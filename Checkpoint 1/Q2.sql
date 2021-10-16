73f88792c51b121f89330c8b8acdc0db5757b900
srikg-msai22 <91036392+srikg-msai22@users.noreply.github.com>
2021-10-15 20:47:11 -0500

    Adding Q2 and Q3

---
+++ Checkpoint 1/Q2.sql
@@ -0,0 +1,2 @@
+-- This question would use information from the tables
+ --the Q6(Allegations(Internal) Disparity) and Q8(Weapons Discharge Disparity)
---
+++ Checkpoint 1/Q3.sql
@@ -0,0 +1,2 @@
+-- This question would use information from the tables
+ --the Q6(Allegations(External) Disparity) and Q8(Weapons Discharge Disparity)
