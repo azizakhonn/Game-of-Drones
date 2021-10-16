68d3e3414e292668de291dd6c74bdfa59239ec6d
srikg-msai22 <91036392+srikg-msai22@users.noreply.github.com>
2021-10-15 19:57:09 -0500

    Adding Q4 and Q8 files

---
+++ Checkpoint 1/Q4.sql
@@ -0,0 +1,21 @@
+--Weapons Discharges based on the police districts
+select
+       unit_description as District_Number_Name,
+       sum(major_award_count) as No_of_Awards,
+       count(dof_id) as No_of_Officers
+from (
+    select
+           dof.id as dof_id,
+           dof.major_award_count,
+           area_beat.unit_description
+    from data_officer as dof join (
+        select
+               *, da.id as da_id, dpb.id as dpb_id, area_id as da_area_id
+        from
+             data_policebeat as dpb
+                 join data_area da
+                     on dpb.area_id = da.id) as area_beat
+        on dof.last_unit_id = area_beat.unit_id
+    where unit_description LIKE '%DISTRICT - %'
+    group by dof_id, unit_description, major_award_count) as officer_unitdesc_award
+group by unit_description
---
+++ Checkpoint 1/Q8.sql
@@ -0,0 +1,27 @@
+--Weapons Discharges based on the police districts
+select
+       unit_description as District_Number_Name,
+       count(officer_injured) as No_of_injured_Officers,
+       count(subject_injured) as No_of_injured_Civilians,
+       sum(total_number_of_shots) as Total_Shots_Fired,
+       sum(number_of_officers_using_firearm) as No_of_Armed_Officers,
+       sum(number_of_weapons_discharged) as No_of_Weapons_Discharged
+from (
+         select area_id,
+                unit_description,
+                officer_injured,
+                subject_injured,
+                total_number_of_shots,
+                number_of_officers_using_firearm,
+                number_of_weapons_discharged
+         from data_area da
+                  join (
+             select *, area_id as view1_area_id
+             from trr_trr as trr
+                      join data_policebeat as dpu
+                           on trr.officer_unit_id = dpu.unit_id) as view1
+                       on view1.view1_area_id = da.id
+         group by area_id, unit_description, officer_injured, subject_injured, total_number_of_shots,
+                  number_of_officers_using_firearm, number_of_weapons_discharged
+     ) as view2
+group by unit_description;
